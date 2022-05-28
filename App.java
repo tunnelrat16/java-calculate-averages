a2305bc5d8cc4510bf3e68327bdac5b5318b9dd5
tunnelrat16 <tjandmatt@hotmail.com>
2022-05-27 17:54:08 -0700

    initial commit

--- App.java
+++ App.java
@@ -1,4 +1,13 @@
 class App {
   public static void main(String args[]){
+    int[] scores = {72, 95, 91, 85, 87, 51, 65, 72, 75, 89};
+    App.median();
+    App.mean();
+    App.highScore();
+    App.lowScore();
+  }
+
+  private static void median(){
+    System.out.println("I love Java");
   }
 }
